e282d18b6b4a0515205491ca92c1fb90ebee1fe5
Clayton Marr <marr.54@buckeyemail.osu.edu>
2022-06-23 18:05:28 -0400

    update extract_taggings.py

--- extract_taggings.py
+++ extract_taggings.py
@@ -11,47 +11,50 @@ optparser.add_option("-s", "--sentence_delim", default='\n', help="Sentence deli
 optparser.add_option("-t", "--tag_flag", default='\t', help="Token delimiting tag from token in input file.")
 optparser.add_option("-f", "--format", default="crf", help="Format of input file. (Currently accepted formats: CRF, CONLL)")
 
-opts, args = optparser.parse_args()
+OPTS, args = optparser.parse_args()
 
 def is_target(line):
-	if opts.prefix:
-		return line[:len(opts.prefix)] == opts.prefix
+	if OPTS.prefix:
+		return line[:len(OPTS.prefix)] == OPTS.prefix
 	return line[0] != '0'
 
-if opts.format.lower() not in ["conll", "crf"]:
+if OPTS.format.lower() not in ["conll", "crf"]:
 	raise Exception("Usage error: only accepted formats are CONLL and CRF currently.")
 
-CONLL = opts.format.lower() == "conll"
+CONLL = OPTS.format.lower() == "conll"
 
 LINES = []
 with open(sys.argv[1]) as f:
-	LINES = f.readlines()
+	LINES += f.readlines()
+
+def tag_tok_tuple(line):
+	tup = line.strip().split(OPTS.tag_flag)
+	return tup[not CONLL] + "_" + tup[CONLL]
 
 def get_context_sentence(line_index):
-	if not opts.context:
+	if not OPTS.context:
 		return ''
 
 	start = line_index - 1
-	while False if start < 0 else LINES[start] != opts.sentence_delim:
+	while False if start < 0 else LINES[start] != OPTS.sentence_delim:
 		start -= 1
 	end = line_index + 1
-	while False if end >= len(LINES) else LINES[end] != opts.sentence_delim:
+	while False if end >= len(LINES) else LINES[end] != OPTS.sentence_delim:
 		end += 1
 
-	sentence = [ ln.split(opts.tag_flag)[CONLL].strip()
-		for ln in LINES[start+1:end] ]
+	sentence = [ tag_tok_tuple(ln) for ln in LINES[start+1:end] ]
 
 	return " ".join(sentence)
 
-output = ["all lines tagged in '"+opts.prefix+"'...:"]
-if not opts.context:
+output = ["all lines tagged in '" + OPTS.prefix + "'...:"]
+if not OPTS.context:
 	output = [ln for ln in LINES if ln.strip() != '']
 	output = [ln for ln in output if is_target(ln)]
 else:
 	output += [("Token\tTag" if CONLL else "Tag\tToken") + "\tSentence"]
 	for i in range(len(LINES)):
 		if is_target(LINES[i]):
-			output += [ LINES[i].strip() + get_context_sentence(i) ]
+			output += [ LINES[i].strip() + "\t" + get_context_sentence(i) ]
 	#output = [("Token\tTag" if CONLL else "Tag\nToken") + "\tSentence"] + [ LINES[i].strip() + get_context_sentence(i) for i in range(len(LINES))]
 
 with open(sys.argv[2], mode='w') as f:
